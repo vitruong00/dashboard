c5991d127d4048f27cee44a814bbd484b8ec2809
Vi Truong <vitruong@Vis-MacBook-Pro.hsd1.tx.comcast.net>
2023-05-27 17:42:29 -0500

    set up components

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { ContextProvider } from "./contexts/ContextProvider";
+
+ReactDOM.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
+  document.getElementById("root")
+);
